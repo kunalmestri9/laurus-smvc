79e0f13e07534b6c8a8b697c601344f385a841eb
Kunal Mestri <kunalmestri@Kunals-MacBook-Pro.local>
2016-12-07 04:49:37 +0530

    Initial Import

---
+++ com/laurus/core/image/exceptions/IImageExceptionType.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IImageExceptionType
+ *
+ * @author Kunal Mestri <kunal@laurus-it.com>
+ */
+class IImageExceptionType {
+    const UNKNOWN= 9999;
+    const MISSING_REQUIRED_PARAMETER = 10000;
+    const USERNAME_ALREADY_EXISTS=10001;
+    const INVALID_CREDENTIALS=10002;
+    const IMAGE_NOT_ADDED=10003;
+}
+
+?>
